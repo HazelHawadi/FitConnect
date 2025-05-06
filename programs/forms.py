4e0e6cb816df26a914326a8af0478575426b81c7
HazelHawadi <hawadihazel@icloud.com>
2025-05-06 11:26:52 +0100

    Add booking confirmation page with Stripe Checkout integration

--- programs/forms.py
+++ programs/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 from .models import Review
+from .models import AvailableDate
 
+class BookingForm(forms.Form):
+    date = forms.ModelChoiceField(queryset=AvailableDate.objects.none(), required=True)
+    sessions = forms.IntegerField(min_value=1, initial=1)
+
+    def __init__(self, *args, **kwargs):
+        program = kwargs.pop('program', None)
+        super().__init__(*args, **kwargs)
+        if program:
+            self.fields['date'].queryset = AvailableDate.objects.filter(program=program, is_booked=False)
 
 class ReviewForm(forms.ModelForm):
     class Meta:
--- programs/urls.py
+++ programs/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from . import views
+from .views import (
+    book_program,
+    confirm_booking,
+    booking_success,
+    create_checkout_session,
+)
 
 urlpatterns = [
     path('', views.program_list, name='program_list'),
     path('<int:pk>/', views.program_detail, name='program_detail'),
-    path('<int:pk>/book/', views.book_program, name='book_program'),
-    path('programs/<int:pk>/book/', views.book_program, name='book_program'),
-    path('booking/confirm/', views.confirm_booking, name='confirm_booking'),
-    path('programs/<int:program_id>/confirm/', views.confirm_booking, name='confirm_booking'),
+    path('book/<int:program_id>/', views.book_program, name='book_program'),
+    path('create-checkout-session/', create_checkout_session, name='create_checkout_session'),
+    path('confirm-booking/', views.confirm_booking, name='confirm_booking'),
+    path('booking/success/', views.booking_success, name='booking_success'),
     path('<int:program_id>/review/', views.add_review, name='add_review'),
     path('<int:program_id>/review/delete/', views.delete_review, name='delete_review'),
 ]
