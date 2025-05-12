1db2997f563e336205de7c9cb36220f5248bcd07
HazelHawadi <hawadihazel@icloud.com>
2025-05-12 03:15:20 +0100

    Add Cache control

--- fitconnect/settings.py
+++ fitconnect/settings.py
@@ -170,6 +170,11 @@ USE_TZ = True
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fitconnectogbucket'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -188,7 +193,7 @@ if 'USE_AWS' in os.environ:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
     
-    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
