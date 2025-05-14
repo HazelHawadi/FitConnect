b403e10bf9838c1cc67c05404dc51be35fed0662
HazelHawadi <hawadihazel@icloud.com>
2025-05-14 02:31:33 +0100

    Style subscription page

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -8,6 +8,7 @@ class Subscription(models.Model):
     renewal_date = models.DateField(null=False)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=timezone.now)
+    start_date = models.DateField(default=timezone.now)
     end_date = models.DateField(null=True, blank=True)
     stripe_subscription_id = models.CharField(max_length=100, blank=True, null=True)
     benefits = models.JSONField(default=list)\
