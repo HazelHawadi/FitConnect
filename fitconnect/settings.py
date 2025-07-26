c403f660e17e525048bddc9fe3665874210e91f3
HazelHawadi <hawadihazel@icloud.com>
2025-07-26 16:27:03 +0100

    Install crispy-bootstrap5 package for forms

--- fitconnect/settings.py
+++ fitconnect/settings.py
@@ -57,7 +57,12 @@ INSTALLED_APPS = [
     
     # Other
     'widget_tweaks',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
