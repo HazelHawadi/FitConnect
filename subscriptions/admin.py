2900888a0bde0f614f52d151b73c55a6ad5596bc
HazelHawadi <hawadihazel@icloud.com>
2025-09-25 20:07:04 +0100

    Register Subscription model in admin

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Subscription
 
-# Register your models here.
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "plan_name", "active", "renewal_date", "stripe_subscription_id")
+    list_filter = ("active", "plan_name")
+    search_fields = ("user__username", "user__email", "stripe_subscription_id")
